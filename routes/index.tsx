df00b383fe9083182ada8b517881441c881b42ef
jonasarce1 <jonasarceg@gmail.com>
2024-02-27 15:21:04 +0100

    fin

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <div>
       <div class="comic-bubble">
-        <p class="comic-text">¡Hola, soy Jonás! Te recomiendo activar el audio y dar permisos de audio y vídeo a tu navegador antes de empezar.</p>
+        <p class="comic-text">¡Hola, soy Jonás! Te recomiendo activar el audio y dar permisos de audio y vídeo a tu navegador antes de empezar. (Puede tardar en cargar la página por favor ten paciencia)</p>
       </div>
       <div>
       <img class = "yo" src = "/images/yo.jpeg" alt = "Un chico muy guapo"></img>
       </div>
       <Start />
       <div class = "comic-bubble-right">
-        <p class = "comic-text">Por cierto, hay un pequeño huevo de pascua escondido en alguna parte de la siguiente página.</p>
+        <p class = "comic-text">¡Hola, yo soy la zanahoria ligeramente interactiva! Por cierto, hay un pequeño huevo de pascua escondido en alguna parte de la siguiente página.</p>
       </div>
       <div>
         <img class = "zanahoria" src = "/images/zanahoria.png" alt = "Yo pero no"></img>
